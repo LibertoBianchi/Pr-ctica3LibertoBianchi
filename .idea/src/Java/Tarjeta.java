d34790149dfe74610e572f249dfd87b7fdfc27f1
Liberto <lbianchi@cifpfbmoll.eu>
2021-05-17 19:07:35 +0200

    Setup

---
+++ .idea/src/Java/Tarjeta.java
@@ -0,0 +1,12 @@
+import javax.naming.Name;
+
+public class Tarjeta {
+    /*NIF;
+    PIN;*/
+    String name = "Pepe";
+
+    public void main(String[] args) {
+        System.out.println(name);
+    }
+
+}
